a37de877f484045f2aac94fee8aae8a83093e955
ramirosarasola <ramiro.sarasola@gmail.com>
2024-06-15 12:32:02 -0300

    format the information.component and center section-title.component

--- components/ui/section-title.component.tsx
+++ components/ui/section-title.component.tsx
@@ -1,6 +1,6 @@
 export const SectionTitleComponent = ({ title }: { title: string }) => {
   return (
-    <h2 className="mb-10 w-fit text-[28px] bg-white shadow-lg text-primary font-bold mx-8 p-6">
+    <h2 className="mb-10 w-fit text-[28px] bg-white shadow-lg text-primary font-bold mx-8 p-6 text-center">
       {title}
     </h2>
   );
