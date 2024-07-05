a3d89a4da3a3addd307370e3f197cc2b5ab47119
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-05 16:12:05 -0300

    success message page edited

--- app/(client)/contratacion/[cotizacionId]/success/page.tsx
+++ app/(client)/contratacion/[cotizacionId]/success/page.tsx
@@ -3,7 +3,7 @@ export default function SuccessPage() {
     <section className="layout_screen mx-auto flex min-h-screen flex-col items-center justify-center p-4">
       <h1 className="text-3xl font-bold">Gracias por confiar en nosotros!</h1>
       <p>En breve nos pondremos en contacto contigo.</p>
-      <p>Te enviamos un mail con los detalles de tu poliza! 😊</p>
+      <p>Te enviaremos un mail con los detalles de tu poliza! 😊</p>
     </section>
-  )
+  );
 }
