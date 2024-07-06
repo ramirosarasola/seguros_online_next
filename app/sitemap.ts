25b76d73ed33d9ec3e8c862908026b418098c31d
ramirosarasola <ramiro.sarasola@gmail.com>
2024-07-05 23:48:56 -0300

    robots and sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,16 @@
-import { getAllPosts } from '@/services';
+import { getAllPosts } from "@/services";
+import { MetadataRoute } from "next";
 
- 
-export default async function sitemap() {
-
-  const baseUrl = 'https://www.segurosonline.com.ar';
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://www.segurosonline.com.ar";
   const response = await getAllPosts();
 
   const blogPosts = response?.map((post: any) => {
+    console.log(`post.node.slug: ${post?.node?.slug}`);
     return {
       url: `${baseUrl}/blog/post/${post?.node?.slug}`,
-      lastModified: new Date(),
-    }
+      lastModified: new Date(post.node.createdAt),
+    };
   });
 
   return [
@@ -18,6 +18,34 @@ export default async function sitemap() {
       url: baseUrl,
       lastModified: new Date(),
     },
-    ...blogPosts
-  ]
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/hogar`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/bici`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/caucion`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/art`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/comercio`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/garantias`,
+      lastModified: new Date(),
+    },
+    ...blogPosts,
+  ];
 }
