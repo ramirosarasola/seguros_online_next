687f66f21d81e329060d778278af1b55c5396524
ramiro <ramiro.sarasola@gmail.com>
2024-10-10 17:43:38 -0300

    remove next folders from disallowing robots to follow

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: ["/*"],
-      disallow: ["/contratacion/*", "/_next/", "/static/", "/public/"],
+      disallow: ["/contratacion/*"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
